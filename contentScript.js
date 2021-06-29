a5c157911ffc744e50863e73fa4931758add619a
Y.Y <dummy@address>
2021-06-29 14:27:53 +0900

    ボタンを押したらコメントを全て「あ」に変更することには成功

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,29 @@
-let comments = document.querySelectorAll("yt-live-chat-text-message-renderer #message")
-for (let i=0; i<comments.length; i++){
-    console.log(comments[i].innerHTML)
+// alert("Hello");
+
+// まずiframeで挿入されてるコメント欄にアクセスさせる
+let chatFrame = document.getElementById('chatframe');
+// コメント欄が見つかるまで繰り返す
+const setIntervalId = setInterval(startObserve, 1000);
+
+function startObserve() {
+  if (chatFrame!==null) {
+    
+    clearInterval(setIntervalId); // コメント欄が見つかったのでsetInterval解除
+
+    // iframeのコメント欄からdocumentを取得 
+    const chatFrameDocument = chatFrame.contentWindow.document;
+    let chats = chatFrameDocument.getElementById("item-offset");
+    let comment_list = chats.querySelectorAll("#message"); // コメントのリスト(HTMLタグ付き)
+    rewrite_comment(comment_list);
+    //alert("ok");
+
+    } else {
+        let chatFrame = document.getElementById('chatframe');
+    }
+}
+
+function rewrite_comment(comment_list) {
+    for (let i=0; i < comment_list.length; i++) {
+        comment_list[i].innerHTML = "あ";
+    }
 }
--- memo.md
+++ memo.md
@@ -3,3 +3,15 @@
 
 Youtubeのコメント欄はiframeらしい？  
 参考になる？https://qiita.com/teahat/items/052a91d69b63b6d7de0c
+
+content_scriptsのmatchesにおける正規表現  
+[Match patterns](https://developer.chrome.com/docs/extensions/mv3/match_patterns/)
+
+コメント欄は遅れて読み込まれるらしいので、コメント欄が見つかるまでループさせる処理を入れる  
+[Youtube用chrome拡張作ってハマったとこ](https://shacchin.github.io/shacchinlog/2019/01/21/20190121/)
+
+コンソールに打ち込む形でコメントの書き換えまではできたが、コンソールを閉じた状態だと実行されない謎  
+とりあえずボタンを押したら実行する形にしてみる  
+
+公式のサンプル集
+https://github.com/GoogleChrome/chrome-extensions-samples
